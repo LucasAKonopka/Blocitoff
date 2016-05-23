123808473bda34a6739f7ef0ab83966806d0a186
Lucas Konopka <lucasakonopka@gmail.com>
2016-05-23 17:58:06 -0500

    Create to-do items complete

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,35 @@
+class ItemsController < ApplicationController
+	
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		@item.user = current_user
+
+		if @item.save
+			flash[:notice] = "Item saved succesfully"
+		else
+			flash[:error] = "Item Failed to Save"
+		end
+		redirect_to user_path(current_user)
+	end
+
+	def destroy
+		@item = Item.find(params[:id])
+		
+		if @item.destroy
+			flash[:notice] = "\"#{@item.name}\" was deleted succesfully."
+			redirect_to action: :index
+		else
+			flash[:error] = "There was an error deleting the Item"
+		end
+	end
+
+	private
+
+ 	def item_params
+    	params.require(:item).permit(:name)
+  	end
+end
